8218773a8a4ee0c76f3d296e4826e5ce62fff8d9
omneyaEid <monaeid9334@gmail.com>
2023-04-26 04:36:02 -0700

    make messenger screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:test_app/login_screen.dart';
 
 import 'home_screen.dart';
+import 'messenger_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-     home: LoginScreen(),
+     home: messengerScreen(),
     );
   }
 }
